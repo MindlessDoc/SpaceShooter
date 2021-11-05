da1fcd7a4099c901d9afc49fd29d66d9bce13f48
tvkoz <tv.kozlov.2002@gmail.com>
2021-11-05 15:27:45 +0300

    Add fragments spawn

--- Assets/Scripts/EnemiesSpawn.cs
+++ Assets/Scripts/EnemiesSpawn.cs
@@ -7,10 +7,15 @@ public class EnemiesSpawn : MonoBehaviour
     [SerializeField] private GameObject _asteroidPrefab;
     [SerializeField] private float _asteroidSpawnTime;
     private float _asteroidNextTime;
+    
+    [SerializeField] private GameObject _fragmentPrefab;
+    [SerializeField] private float _fragmentSpawnTime;
+    private float _fragmentNextTime;
 
     void Start()
     {
         _asteroidNextTime = _asteroidSpawnTime;
+        _fragmentNextTime = _fragmentSpawnTime;
     }
 
 
@@ -21,6 +26,13 @@ public class EnemiesSpawn : MonoBehaviour
             Instantiate(_asteroidPrefab, new Vector3(Random.Range(-2.2f, 2.2f), 5.7f, transform.position.z),
                 Quaternion.identity);
             _asteroidNextTime = Time.time + _asteroidSpawnTime;
+        }
+        
+        if (_fragmentNextTime < Time.time)
+        {
+            Instantiate(_fragmentPrefab, new Vector3(Random.Range(-2.2f, 2.2f), 5.7f, transform.position.z),
+                Quaternion.identity);
+            _fragmentNextTime = Time.time + _fragmentSpawnTime;
         }
     }
 }
---
+++ Assets/Scripts/Fragment.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fragment : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3.0f;
+
+    [SerializeField] private AudioClip _loot;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if(transform.position.y < -6)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            AudioSource.PlayClipAtPoint((_loot), transform.position, 1.0f);
+            Destroy(gameObject);
+            PlayerPrefs.SetInt("Fragment", PlayerPrefs.GetInt("Fragment"));
+        }
+    }
+}
